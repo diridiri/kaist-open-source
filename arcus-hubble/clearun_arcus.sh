66cfec45de2591ffb65d7e107cebe4744b1ec783
diridiri <junharin@kaist.ac.kr>
2016-06-04 21:17:41 +0900

    Dockerfile

---
+++ arcus-hubble/clearun_arcus.sh
@@ -0,0 +1,17 @@
+./arcus.sh quicksetup conf/local.sample.json
+#sleep 4
+#~/hubblemon/clearun_hubblemon.sh
+#./arcus.sh deploy conf/local.sample.json
+#./arcus.sh zookeeper init
+#./arcus.sh zookeeper start
+#./arcus.sh memcached register conf/local.sample.json
+#./arcus.sh memcached start test
+
+#./arcus.sh zookeeper stat
+#./arcus.sh memcached list test
+#while :
+#do
+#    sleep 10
+#    ./arcus.sh memcached list test
+#    date
+#done
--- arcus-hubble/start_arcus-hubble.sh
+++ arcus-hubble/start_arcus-hubble.sh
@@ -1,1 +1,1 @@
-docker run -it -p 8080:8080 --rm=false --name=arcus-hubble diri/arcus-hubble_image
+docker run -it -p 25828:25828 -p 25829:25829 -p 25832:25832 -p 2181:2181 -p 3000:3000 -p 8080:8080 -p 8007:8007 --rm --name=arcus-hubble diri/arcus-hubble_image
